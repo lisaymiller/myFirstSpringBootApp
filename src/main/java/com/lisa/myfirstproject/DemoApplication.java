6c2154cfa83bba6ca0a07d73b464b212d2b0508d
Lisa Miller <lisayevonne@outlook.com>
2020-06-18 15:04:59 -0400

    index and css added and displayed

--- src/main/java/com/lisa/myfirstproject/DemoApplication.java
+++ src/main/java/com/lisa/myfirstproject/DemoApplication.java
@@ -1,3 +1,11 @@
+/*- Requirements for the application: 
+  - Add in Styling (through a CSS Sheet)
+  - Investigate HTML Forms & add a form 
+  - Connect multiple pages 
+  - Add in images to your web pages
+- **Extra**:
+  - A Database that saves the information from your HTML form */
+
 package com.lisa.myfirstproject;
 
 import org.springframework.boot.SpringApplication;
