d92326acfc716edf14bd15ca0b53f148bb42221d
Lisa Miller <lisayevonne@outlook.com>
2020-06-18 15:21:09 -0400

    multi page works

--- src/main/java/com/lisa/myfirstproject/controllers/indexController.java
+++ src/main/java/com/lisa/myfirstproject/controllers/indexController.java
@@ -9,4 +9,9 @@ public class indexController {
     public String index() {
         return "index.html";
     }
+
+    @RequestMapping("/form")
+    public String form() {
+        return "form.html";
+    }
 }
